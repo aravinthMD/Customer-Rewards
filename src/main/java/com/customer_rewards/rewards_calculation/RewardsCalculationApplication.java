9e8ab3b50152c40d7ed3988b803c6ac31502e445
Aravinth <aravinth12321@gmail.com>
2025-05-10 13:26:33 +0530

    Initializing the Project

---
+++ src/main/java/com/customer_rewards/rewards_calculation/RewardsCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.customer_rewards.rewards_calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsCalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customer_rewards/rewards_calculation/RewardsCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer_rewards.rewards_calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
